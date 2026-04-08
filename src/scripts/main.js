c38dacad8671a4e587291d8c72dd63ed12c75889
Alternate001 <artem.kushko@publizer.de>
2026-04-08 12:14:29 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.style.position = 'relative';
+spider.style.position = 'absolute';
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const wallBond = wall.getBoundingClientRect();
+  let topBox = e.clientY - wallBond.top - spider.offsetHeight / 2;
+  let leftBox = e.clientX - wallBond.left - spider.offsetWidth / 2;
+
+  if (topBox > wallBond.height - spider.offsetHeight) {
+    topBox = wallBond.height - spider.offsetHeight;
+  } else if (topBox < 0) {
+    topBox = 0;
+  }
+
+  if (leftBox > wallBond.width - spider.offsetWidth) {
+    leftBox = wallBond.width - spider.offsetWidth;
+  } else if (leftBox < 0) {
+    leftBox = 0;
+  }
+
+  spider.style.top = topBox + 'px';
+  spider.style.left = leftBox + 'px';
 });
