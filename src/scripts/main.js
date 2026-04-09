ff1efbff7a8ecd5c9484658497a6aedcead561b8
Alternate001 <artem.kushko@publizer.de>
2026-04-09 11:03:38 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,23 +6,25 @@ const spider = document.querySelector('.spider');
 wall.style.position = 'relative';
 spider.style.position = 'absolute';
 
+const clamp = (value, min, max) => {
+  return Math.min(Math.max(value, min), max);
+};
+
 wall.addEventListener('click', (e) => {
   const wallBond = wall.getBoundingClientRect();
-  let topBox = e.clientY - wallBond.top - spider.offsetHeight / 2;
-  let leftBox = e.clientX - wallBond.left - spider.offsetWidth / 2;
 
-  if (topBox > wallBond.height - spider.offsetHeight) {
-    topBox = wallBond.height - spider.offsetHeight;
-  } else if (topBox < 0) {
-    topBox = 0;
-  }
+  const leftBox = e.clientX - wallBond.left - spider.offsetWidth / 2;
+  const maxLeft = wallBond.width - spider.offsetWidth;
+  const allowedLeft = clamp(leftBox, 0, maxLeft);
 
-  if (leftBox > wallBond.width - spider.offsetWidth) {
-    leftBox = wallBond.width - spider.offsetWidth;
-  } else if (leftBox < 0) {
-    leftBox = 0;
-  }
+  const topBox = e.clientY - wallBond.top - spider.offsetHeight / 2;
+  const maxTop = wallBond.height - spider.offsetHeight;
+  const allowedTop = clamp(topBox, 0, maxTop);
 
-  spider.style.top = topBox + 'px';
-  spider.style.left = leftBox + 'px';
+  spider.style.top = allowedTop + 'px';
+  spider.style.left = allowedLeft + 'px';
+  console.log (`allowed top ${allowedTop}`);
+  console.log (`click top ${topBox}`);
+  console.log (`allowed left ${allowedLeft}`);
+  console.log (`click left ${leftBox}`);
 });
